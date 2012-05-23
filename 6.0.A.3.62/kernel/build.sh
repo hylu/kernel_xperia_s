152056f5be32c16df267545a93fdabfe28fe2c1d
Hsinyi Lu <hylu2000@hotmail.com>
2012-05-23 10:19:15 +0800

    set "LOCALVERSION=" in the script.

--- 6.0.A.3.62/kernel/build.sh
+++ 6.0.A.3.62/kernel/build.sh
@@ -1,1 +1,1 @@
-ARCH=arm CROSS_COMPILE=/home/hslu/workspace/htc_buzz/cm7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- make -j4
+LOCALVERSION= ARCH=arm CROSS_COMPILE=/home/hslu/workspace/htc_buzz/cm7/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- make -j4
